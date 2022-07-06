12e6d921803ae41a483833a89f948acff8dec403
Seabass5701 <40925538+Seabass5701@users.noreply.github.com>
2022-07-06 04:18:07 -0400

    Update android_debloater.sh

--- android_debloater.sh
+++ android_debloater.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env sh
 
 
-# A debloat script for Android devices.
+# a debloat script for android devices.
 #
-# Please do research before debloating, as removing the wrong APKs can result
-# in data-loss/corruption and/or OS-reinstallation. Consequently, a backup is
+# please do research before debloating, as removing the wrong apks can result
+# in data-loss/corruption and/or os-reinstallation. consequently, a backup is
 # highly recommended before proceeding further.
 #
-# Rather than having a predefined list of APKs labeled as bloatware, this
+# rather than having a predefined list of apks labeled as bloatware, this
 # script allows the user to pass a text-file in the working directory of the
 # script, with the name pkg.list (or with the value of var: pkg_list)
 #
-# If you are unfamiliar with the process of debloating, please do a web-search
+# if you are unfamiliar with the process of debloating, please do a web-search
 # on suspect packages, before deleting them, as there are often explanations of
-# (otherwise unknown) APKs, which may be found on your Android device. Bloatware
-# may also be [OEM|Carrier|etc..]-specific.
+# (otherwise unknown) apks, which may be found on your Android device.
 
 
 
-# Silence output (to stdout(1) & stderr(2), or both)
+# silence output (to stdout(1) & stderr(2), or both)
 no_stdout() { $@ 1>/dev/null; }
 no_stderr() { $@ 2>/dev/null; }
 no_out() { $@ 1>/dev/null 2>/dev/null; }
 
 
-# By default, the "action" variable will be
+# by default, the "action" variable will be
 # set to the first argument passed to script ($1).
 no_stderr ${action:=$1}
 
 
-# ALL APK-Files (which are to be removed)
+# all apk files (which are to be removed)
 # will be kept here, in the following format:
 #
 # package:com.android.bluetooth
@@ -38,22 +37,22 @@ no_stderr ${action:=$1}
 
 
 
-# Display help
+# display help
 display_help() {
 	echo "usage: \`env [pkg_list=\"<uri-path to file>\"] $0 [<action>]\`\n\nenv variables:\npkg_list - path to pkg_list file\n\nActions:\ndebloat - remove packages\nrestore - restore [deleted] packages\nhelp - display this menu"
 	return 1
 }
 
 
-# Return error when action is not recognized
-# by the script.
+# return error when action is not recognized
+# by the script
 action_not_found() {
 	echo "invalid action [or no argument was passed to command-line]\nvalid actions: <debloat|restore|help>" >&2
 	return 1
 }
 
 
-# Return error when ADB is not installed
+# return error when adb is not installed
 # (or added to runtime exec-path)
 adb_not_found() {
 	echo "adb could not be found" >&2
@@ -61,44 +60,44 @@ adb_not_found() {
 }
 
 
-# Return error when no device is recognizable
-# by, or connected to ADB.
+# return error when no device is recognizable
+# by, or connected to adb
 dev_not_connected() {
 	echo "check that android device is:\n\n1): turned on\n2): plugged into your pc (via usb)\n3): connected to your pc (via adb w/ usb debugging enabled)" >&2
 	exit 1
 }
 
 
-# Return error when no package list is found,
-# containing the list of APK-Files to remove.
+# return error when no package list is found,
+# containing the list of apk files to remove
 pkg_list_not_found() {
 	echo "a package list containing the list of apks you wish to remove/restore has not been found:\n\n1): run \`adb shell pm list packages\` to obtain apk list\n2): add the apks you wish to remove/restore to \"$pkg_list\"\n\n(to change package list filename, \`env pkg_list=\"<path-to-file>\" $0 [restore|debloat]\`)\n(to fetch the package list from a device directly, and transfer it to a textfile, \`(adb shell pm list packages) > \"<path-to-file>\"\`)" >&2
 	return 1
 }
 
 
-# Return error when package list file is
-# improperly formatted.
+# return error when package list file is
+# improperly formatted
 pkg_list_invalid_format() {
 	echo "package list found, however the file is likely improperly-formatted (or empty).\npackages listed inside of \"$pkg_file\", should have the format:\n[package:]com.android.chrome" >&2
 	return 1
 }
 
 
-# Return error when adb state is invalid
+# return error when adb state is invalid
 adb_state_invalid() {
 	echo "invalid adb-state: $state" >&2
 	return 1
 }
 
 
-# Check whether ADB can be ran.
+# check whether adb can be ran
 check_adb() {
 	test -e "/usr/local/bin/adb" -o -e "/usr/bin/adb" || adb_not_found
 }
 
 
-# Check ADB (connection) state.
+# check adb (connection) state
 check_adb_state() {
 	# start the adb daemon
 	no_out adb start-server
@@ -111,32 +110,32 @@ check_adb_state() {
 }
 
 
-# Check pkg_list
+# check pkg_list
 check_pkg_list() {
 	[ -f "$pkg_list" ] || pkg_list_not_found
 }
 
 
-# Obtain (all) APKs included within APK-list.
+# obtain (all) apks included within apk list
 get_apk_all() {
 	no_stderr grep '^[package:?.*$]' $pkg_list || pkg_list_invalid_format
 }
 
 
-# Obtain the number of APKs included within APK-list.
+# obtain the number of apks included within apk list
 get_linecount() {
 	no_stderr export linecount="`get_apk_all | wc -l`"
 }
 
 
-# Obtain the current APK in the list
+# obtain the current apk in the list
 get_apk_curr() {
 	curr_apk="`get_apk_all | sed -n "$i"p`" && \
 		export curr_apk=${curr_apk##package:}
 }
 
 
-# Check action being performed.
+# check action being performed
 check_action() {
 	case "$action" in
 		debloat|restore) continue ;;
@@ -146,7 +145,7 @@ check_action() {
 }
 
 
-# Perform [requested] action
+# perform [requested] action
 do_action() {
 	case "$action" in
 		debloat) no_out adb shell pm uninstall --user 0 $curr_apk ;;
@@ -155,19 +154,19 @@ do_action() {
 }
 
 
-# Return success msg
+# return success msg
 action_success() {
 	echo "[+] successfully ${action%%e}ed: $curr_apk [$i/$linecount]"
 }
 
 
-# Return error msg
+# return error msg
 action_error() {
 	echo "[-] error ${action%%e}ing: $curr_apk [$i/$linecount]" >&2
 }
 
 
-# Perform action on each APK included within APK-list,
+# perform action on each apk included within apk list,
 # sequentially.
 do_action_apks() {
 	i=0 && {
